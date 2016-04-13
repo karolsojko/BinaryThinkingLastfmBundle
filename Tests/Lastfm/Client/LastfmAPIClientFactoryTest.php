54ec13e16495dfd361846cd6d7baf87fe2d4ce74
black-bullet <blackbullet@i.ua>
2016-04-13 14:47:22 +0300

    Added tests for ApiClientFactory

--- Tests/Lastfm/Client/LastfmAPIClientFactoryTest.php
+++ Tests/Lastfm/Client/LastfmAPIClientFactoryTest.php
@@ -12,7 +12,7 @@ use BinaryThinking\LastfmBundle\Lastfm\Client\LastfmAPIClientFactory;
 class LastfmAPIClientFactoryTest extends \PHPUnit_Framework_TestCase
 {
     protected $apiKey;
-    
+
     protected $apiSecret;
 
     protected function setUp()
@@ -21,16 +21,16 @@ class LastfmAPIClientFactoryTest extends \PHPUnit_Framework_TestCase
         $this->apiKey = 'test';
         $this->apiSecret = 'testSecret';
     }
-    
+
     /**
      * @expectedException Symfony\Component\DependencyInjection\Exception\InvalidArgumentException
      */
     public function testGetInvalidClient()
     {
-        $client = LastfmAPIClientFactory::getClient('someclient', 
+        $client = LastfmAPIClientFactory::getClient('someclient',
                 $this->apiKey, $this->apiSecret);
     }
-    
+
     public function testGetClientForAlbum()
     {
         $albumClient = LastfmAPIClientFactory::getClient('album',
@@ -38,40 +38,53 @@ class LastfmAPIClientFactoryTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('BinaryThinking\LastfmBundle\Lastfm\Client\Method\AlbumMethodsClient',
                 $albumClient, 'wrong client delivered');
     }
-    
+
     public function testGetAlbumClient()
     {
         $albumClient = LastfmAPIClientFactory::getAlbumClient($this->apiKey, $this->apiSecret);
         $this->assertInstanceOf('BinaryThinking\LastfmBundle\Lastfm\Client\Method\AlbumMethodsClient',
                 $albumClient, 'wrong client delivered');
     }
-    
+
     public function testGetClientForArtist()
     {
         $artistClient = LastfmAPIClientFactory::getClient('artist', $this->apiKey, $this->apiSecret);
         $this->assertInstanceOf('BinaryThinking\LastfmBundle\Lastfm\Client\Method\ArtistMethodsClient',
                 $artistClient, 'wrong client delivered');
-    }    
-    
+    }
+
     public function testGetArtistClient()
     {
         $artistClient = LastfmAPIClientFactory::getArtistClient($this->apiKey, $this->apiSecret);
         $this->assertInstanceOf('BinaryThinking\LastfmBundle\Lastfm\Client\Method\ArtistMethodsClient',
                 $artistClient, 'wrong client delivered');
     }
-    
+
     public function testGetClientForTag()
     {
         $tagClient = LastfmAPIClientFactory::getClient('tag', $this->apiKey, $this->apiSecret);
         $this->assertInstanceOf('BinaryThinking\LastfmBundle\Lastfm\Client\Method\TagMethodsClient',
                 $tagClient, 'wrong client delivered');
     }
-    
+
     public function testGetTagClient()
     {
         $tagClient = LastfmAPIClientFactory::getTagClient($this->apiKey, $this->apiSecret);
         $this->assertInstanceOf('BinaryThinking\LastfmBundle\Lastfm\Client\Method\TagMethodsClient',
                 $tagClient, 'wrong client delivered');
     }
-    
+
+    public function testGetGeoClient()
+    {
+        $geoClient = LastfmAPIClientFactory::getGeoClient($this->apiKey, $this->apiSecret);
+        $this->assertInstanceOf('BinaryThinking\LastfmBundle\Lastfm\Client\Method\GeoMethodsClient',
+            $geoClient, 'wrong client delivered');
+    }
+
+    public function testGetClientForGeo()
+    {
+        $geoClient = LastfmAPIClientFactory::getClient('geo', $this->apiKey, $this->apiSecret);
+        $this->assertInstanceOf('BinaryThinking\LastfmBundle\Lastfm\Client\Method\GeoMethodsClient',
+            $geoClient, 'wrong client delivered');
+    }
 }
